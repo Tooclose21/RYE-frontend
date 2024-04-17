308f0f8112b177efa1d8232a625b6b9ef074430f
Tooclose21 <alasuper.as@gmail.com>
2024-04-17 09:41:01 +0200

    navbar

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Navbar from "./components/Navbar";
 
 test('renders learn react link', () => {
   render(<App />);
