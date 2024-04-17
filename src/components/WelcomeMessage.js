0fac3cb6da49e370e06799a3beeb1149b5d2eb41
Tooclose21 <alasuper.as@gmail.com>
2024-04-17 15:35:46 +0200

    Welcome page with ghost

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -1,13 +1,16 @@
+import blueGhost from '../images/blueGhost.png';
 import '../styles.css';
 import React from "react"
+import logo from "../images/logo.svg";
 
 export default function WelcomeMessage() {
     return (
         <welcome>
             <div className={"message--text"}>
-            <h2 className="message--text1"> Welcome back!</h2>
-            <h2 className="message--text2">What would you like to do today?</h2>
+            <h1 className="message--text1"> Welcome back!</h1>
+            <h1 className="message--text2">What would you like to do today?</h1>
             </div>
+            <img src={blueGhost} className="welcome--img" alt="Blue ghost"/>
         </welcome>
     )
 }
