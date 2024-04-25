2ceb6098f65fabd661f7a56a54cc56371c968805
Tooclose21 <alasuper.as@gmail.com>
2024-04-25 12:21:15 +0200

    Mimic from name page added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ChooseGameTimeRfP from "./pages/ChooseGameTimeRfP";
 import ChooseGameTimeMfP from "./pages/ChooseGameTimeMfP";
 import ChooseGameTimeMfN from "./pages/ChooseGameTimeMfN";
-
+import CharacterCustomization from "./pages/CharacterCustomization";
+import MimicFromName from "./pages/MimicFromName";
 
 const rootElement = document.getElementById("root");
     ReactDOM.render(
@@ -19,6 +20,8 @@ const rootElement = document.getElementById("root");
             <Route path="/ChooseGameTimeRfP" element={<ChooseGameTimeRfP/>} />
             <Route path="/ChooseGameTimeMfP" element={<ChooseGameTimeMfP/>}/>
             <Route path="/ChooseGameTimeMfN" element={<ChooseGameTimeMfN/>}/>
+            <Route path="/CharacterCustomization" element={<CharacterCustomization/>}/>
+            <Route path="MimicFromName" element={<MimicFromName/>}/>
         </Routes>
     </BrowserRouter>,
         rootElement
--- src/pages/ChildWelcomePage.js
+++ src/pages/ChildWelcomePage.js
@@ -15,7 +15,7 @@ function ChildWelcomePage() {
         <BlueGhost />
           <Link to="/ChooseGameMode"><Button loc={{position: 'absolute', top: '45%', left: '60%'}} color="#88CAFC">Play</Button></Link>
           <Button loc={{position: 'absolute', top: '55%', left: '60%'}} color="#88CAFC">Tasks</Button>
-          <Button loc={{position: 'absolute', top: '65%', left: '60%'}} color="#88CAFC">Customize</Button>
+          <Link to="/CharacterCustomization"><Button loc={{position: 'absolute', top: '65%', left: '60%'}} color="#88CAFC">Customize</Button></Link>
           <Button loc={{position: 'absolute', top: '75%', left: '60%'}} color="#88CAFC">My stats</Button>
       </div>
   );
