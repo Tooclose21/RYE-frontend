eb691e045c7175888d0a48fb543b499f503cf353
Tooclose21 <alasuper.as@gmail.com>
2024-06-23 09:57:03 +0200

    Plot added

--- src/pages/Stats/Child/StatsForChild.js
+++ src/pages/Stats/Child/StatsForChild.js
@@ -4,13 +4,29 @@ import blueGhost from "../../../images/blueGhost.png";
 import './StatsForChild.css';
 import ComboBox from "../../../components/ComboBox";
 import Rectangle from "../../../components/Rectangle";
+import Plot from "react-plotly.js"
 
 
 function StatsForChild() {
-    const AmountOptions = Array.from({ length: 5 }, (_, index) => index + 1);
+    const AmountOptions = Array.from({length: 5}, (_, index) => index + 1);
+    const modes= ["MIMIC_FROM_NAME",
+        "MIMIC_FROM_PICTURE",
+        "RECOGNIZE_FROM_PICTURE"]
+    const dict = {
+        '2024-06-06':
+            6.5,
+        '2024-06-07':
+            4,
+        '2024-06-08':
+            2,
+        '2024-06-09':
+            5.333333333333333,
+        '2024-06-12':
+            5.666666666666667,
+    }
     return (
         <div style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#D2EBFF'}}>
-            <LightBlueNavbar />
+            <LightBlueNavbar/>
             <div className={"statistics--child--message"}>
                 <h1 className={"child--statistics--text"}>
                     <span style={{fontSize: "2vw"}}>Welcome to Statistics!</span> <br/>
@@ -18,17 +34,31 @@ function StatsForChild() {
                 </h1>
             </div>
             <div className={"child--general--message"}
-                 >
+            >
                 <h1 className={"child--general--statistics--text"}>
                     General
                 </h1>
             </div>
-            <Rectangle className="child--rectangle1"/>
-            <h1 className={"child--period--statistics--text"}>Period</h1>
-            <Rectangle className="child--rectangle3" />
-            <h1 className={"child--score--statistics--text"}>Score</h1>
-            <Rectangle className="child--rectangle2"/>
-            <h1 className={"child--practice--statistics--text"}>Practice time</h1>
+            <Plot className="stats-child-plot"
+            data = {[{
+                x: Object.keys(dict),
+                y: Object.values(dict),
+                type: "scatter",
+                mode: "lines+markers"
+            }]}
+            layout = {{title: "General statistics",
+                xaxis: { title: "Date" },
+                yaxis: { title: "Points" }}}
+            config={{ displayModeBar: false }}></Plot>
+            <ComboBox className="comboBox-stats" options={modes} style={{position:'absolute',top: "90%", left: "65%"}}/>
+
+
+            {/*<Rectangle className="child--rectangle1"/>*/}
+            {/*<h1 className={"child--period--statistics--text"}>Period</h1>*/}
+            {/*<Rectangle className="child--rectangle3"/>*/}
+            {/*<h1 className={"child--score--statistics--text"}>Score</h1>*/}
+            {/*<Rectangle className="child--rectangle2"/>*/}
+            {/*<h1 className={"child--practice--statistics--text"}>Practice time</h1>*/}
             <img src={blueGhost} className="child--ghost--statistics--img" alt="Blue ghost"/>
         </div>
     );
